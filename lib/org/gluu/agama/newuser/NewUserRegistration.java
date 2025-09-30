83473c46b60d1d74b7b05c33051b75fe424bab5a
AutomateCentroxy <155064161+AutomateCentroxy@users.noreply.github.com>
2025-09-30 18:46:42 +0530

    feat: add NewUserRegistration.java

---
+++ lib/org/gluu/agama/newuser/NewUserRegistration.java
@@ -0,0 +1,34 @@
+package org.gluu.agama.user;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.gluu.agama.newregistration.JansUserRegistration;
+
+
+
+public abstract class UserRegistration {
+    public abstract Map<String, Object> validateInputs(Map<String, String> profile);
+
+    public abstract String addNewUser(Map<String, String> profile) throws Exception;
+
+    // public abstract boolean usernamePolicyMatch(String userName);
+
+    // public abstract boolean passwordPolicyMatch(String userPassword);
+
+    public abstract String sendEmail(String to, String lang);
+
+    public abstract String sendOTPCode(String phone);
+
+    public abstract boolean validateOTPCode(String phone, String code);
+
+    public abstract boolean markPhoneAsVerified(String userName);
+    
+    // public static UserRegistration getInstance(){
+    //     return  JansUserRegistration.getInstance();
+    // } 
+    
+    public static UserRegistration getInstance(HashMap config) {
+        return new JansUserRegistration(config);
+    }
+}
